dd27753180c3adfc0f800b0f8690cd7282141f4f
yato0218 <touyamavv8@gmail.com>
2026-03-30 20:36:39 +0900

    プレイヤーの斜め移動に制限をかけた。斜め移動が速くなるのは面白いけど、とりあえずそうする

--- scroll_game.py
+++ scroll_game.py
@@ -1,4 +1,5 @@
 import pyxel
+import math
 
 screen_width = 400
 screen_height = 300
@@ -9,18 +10,49 @@ SCENE_GAME_OVER = 2
 
 PLAYER_SIZE_X = 8
 PLAYER_SIZE_Y = 8
+PLAYER_SPEED = 5
 
 class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.player_frame_num = 4
+        self.frame_interval = 30
+        
+        
     def update(self):
-        pass
+        #sin cos でルート2走法を禁止できるんじゃね？ sin^2+cos^2 = 1とか？
+        direction_x = 0
+        direction_y = 0
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            direction_x += 1
+        if pyxel.btn(pyxel.KEY_LEFT):
+            direction_x += -1
+        if pyxel.btn(pyxel.KEY_UP):
+            direction_y += -1
+        if pyxel.btn(pyxel.KEY_DOWN):
+            direction_y += 1
+
+        lengh = math.sqrt(math.pow(direction_x,2) + math.pow(direction_y,2))
+
+        if direction_x != 0:
+            self.x += (direction_x  / lengh) * PLAYER_SPEED
+        if direction_y != 0:
+            self.y += (direction_y  / lengh) * PLAYER_SPEED
+
+
     def draw(self):
-        pyxel.blt(self.x, self.y, 0, 0, 0, PLAYER_SIZE_X, PLAYER_SIZE_Y, pyxel.COLOR_BLACK)
+
+        self.player_current_frame = (pyxel.frame_count // self.frame_interval) % self.player_frame_num
+        u = self.player_current_frame * 8
+        pyxel.blt(self.x, self.y, 0, u, 0, PLAYER_SIZE_X, PLAYER_SIZE_Y, pyxel.COLOR_BLACK)
         # pyxel.blt(x, y, img, u, v, w, h, colkey=None, rotate=0, scale=1)
         #text(x, y, s, col, font=None)
-        pyxel.text(10,10, f"Game Start", pyxel.COLOR_LIME)
+        pyxel.text(10,10, f"self.x : {self.x}", pyxel.COLOR_LIME)
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            pyxel.text(10,10, f"R", pyxel.COLOR_LIME)
+        if pyxel.btn(pyxel.KEY_LEFT):
+            pyxel.text(20,10, f"L", pyxel.COLOR_LIME)
 
 class Background:
     def __init__(self):
