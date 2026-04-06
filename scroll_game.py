cac35ed1d45df2cd5b5a9ab6d9f83bd99d6b71df
yato0218 <touyamavv8@gmail.com>
2026-04-06 17:06:05 +0900

    entities_is_crashという引数2つの当たり判定を行う関数を作成した

--- scroll_game.py
+++ scroll_game.py
@@ -1,5 +1,6 @@
 import pyxel
 import math
+import random
 
 screen_width = 400
 screen_height = 300
@@ -12,6 +13,11 @@ bullets = []
 BULLET_SIZE_X = 8
 BULLET_SIZE_Y = 8
 BULLET_SPEED_X = 10
+
+enemies = []
+ENEMIES_MAX_NUM = 4
+ENEMY_SIZE_X = 20
+ENEMY_SIZE_Y = 20
 
 PLAYER_SIZE_X = 16
 PLAYER_SIZE_Y = 16
@@ -31,6 +37,18 @@ def entities_draw(entities):
             entity.draw()
             pyxel.text(10, 40, f"bullets: {len(bullets)}", pyxel.COLOR_GREEN)
 
+def entities_is_crash(entities_1, entities_2):
+        for entity_1 in entities_1:
+            for entity_2 in entities_2:
+                if not entity_1.is_alive or not entity_2.is_alive:
+                    continue
+                if (entity_1.x <= entity_2.x + entity_2.w and
+                    entity_2.x <= entity_1.x + entity_1.w and
+                    entity_1.y <= entity_2.y + entity_2.h and
+                    entity_2.y <= entity_1.y + entity_1.h):
+                    entity_1.is_alive = False
+                    entity_2.is_alive = False
+
 def entities_cleanup(entities):
     # entities[:]は引数entitiesのリストの中身を全部出して,右側の値([]の中身)をentitiesのリストに入れなおすというものである
     # = [a for b in c if xxx] について. これは cというリストから1つ取り出し,bと名付ける. その後,xxxの条件に当てはまるものをaに変換してに入れる
@@ -41,10 +59,13 @@ def entities_cleanup(entities):
     # entities[:] = [entity.x for entity in entities if entity.is_alive]
 
 
+
 class Bullet:
     def __init__(self, x, y, right):
         self.x = x
         self.y = y
+        self.w = BULLET_SIZE_X
+        self.h = BULLET_SIZE_Y
         self.is_right = right
         self.is_alive = True
         if self.is_right:
@@ -68,15 +89,30 @@ class Bullet:
     def draw(self):
         if self.is_alive:
             if self.is_right:
-                pyxel.blt(self.x, self.y, 1, 0, 0, BULLET_SIZE_X, BULLET_SIZE_Y, pyxel.COLOR_BLACK)
+                pyxel.blt(self.x, self.y, 1, 0, 0, self.w, self.h, pyxel.COLOR_BLACK)
             else:
-                pyxel.blt(self.x, self.y, 1, 0, 0, -BULLET_SIZE_X, BULLET_SIZE_Y, pyxel.COLOR_BLACK)
+                pyxel.blt(self.x, self.y, 1, 0, 0, -self.w, self.h, pyxel.COLOR_BLACK)
 
+class Enemy:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.w = ENEMY_SIZE_X
+        self.h = ENEMY_SIZE_Y
+        self.is_alive = True
+        enemies.append(self)
+    def update(self):
+        pass
+           
+    def draw(self):
+        pyxel.rect(self.x, self.y, self.w, self.h, pyxel.COLOR_LIGHT_BLUE)
 
 class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.w = PLAYER_SIZE_X
+        self.h = PLAYER_SIZE_Y
         self.player_frame_num = 4
         self.frame_interval = 8
         self.vy = 0
@@ -131,10 +167,15 @@ class Player:
 
         if pyxel.btn(pyxel.KEY_SPACE):
             if self.is_right:
-                Bullet(self.x + PLAYER_SIZE_X, self.y + PLAYER_SIZE_Y // 4, self.is_right)
+                Bullet(self.x + self.w, self.y + self.h // 4, self.is_right)
             else:
-                Bullet(self.x - PLAYER_SIZE_X // 2, self.y + PLAYER_SIZE_Y // 4, self.is_right)
+                Bullet(self.x - self.w // 2, self.y + self.h // 4, self.is_right)
 
+        if pyxel.frame_count % 60 == 0:
+            if len(enemies) <= ENEMIES_MAX_NUM:
+                Enemy(random.randint(screen_width // 10 * 7, screen_width // 10 * 9),
+                      random.randint(screen_height // 10 * 5, screen_height // 10 * 7))
+                 
 
         
         
@@ -144,15 +185,15 @@ class Player:
         if self.vx == 0 and self.is_right and self.vy == 0:
             self.player_current_frame = (pyxel.frame_count // self.frame_interval) % self.player_frame_num
             u = self.player_current_frame * 16
-            pyxel.blt(self.x, self.y, 0, u, 0, PLAYER_SIZE_X, PLAYER_SIZE_Y, pyxel.COLOR_BLACK)
+            pyxel.blt(self.x, self.y, 0, u, 0, self.w, self.h, pyxel.COLOR_BLACK)
         elif self.vx == 0 and not self.is_right and self.vy == 0:
             self.player_current_frame = (pyxel.frame_count // self.frame_interval) % self.player_frame_num
             u = self.player_current_frame * 16
-            pyxel.blt(self.x, self.y, 0, u, 0, - PLAYER_SIZE_X, PLAYER_SIZE_Y, pyxel.COLOR_BLACK)
+            pyxel.blt(self.x, self.y, 0, u, 0, - self.w, self.h, pyxel.COLOR_BLACK)
         elif self.vx >= 0 and self.is_right:
-            pyxel.blt(self.x, self.y, 0, 0, 0, PLAYER_SIZE_X, PLAYER_SIZE_Y, pyxel.COLOR_BLACK)
+            pyxel.blt(self.x, self.y, 0, 0, 0, self.w, self.h, pyxel.COLOR_BLACK)
         elif self.vx <= 0 and not self.is_right:
-            pyxel.blt(self.x, self.y, 0, 0, 0, -PLAYER_SIZE_X, PLAYER_SIZE_Y, pyxel.COLOR_BLACK)#キャラの向きを左右反転させるため-PLAYER_SIZE_X
+            pyxel.blt(self.x, self.y, 0, 0, 0, - self.w, self.h, pyxel.COLOR_BLACK)#キャラの向きを左右反転させるため-self.w
         # pyxel.blt(x, y, img, u, v, w, h, colkey=None, rotate=0, scale=1)
         #text(x, y, s, col, font=None)
         pyxel.text(10,10, f"self.x : {self.x}", pyxel.COLOR_LIME)
@@ -196,6 +237,7 @@ class App:
     def update_title_scene(self):
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
             self.scene = SCENE_PLAY
+
     def update_play_scene(self):
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
             self.scene = SCENE_GAME_OVER
@@ -203,6 +245,8 @@ class App:
         
         self.background.update()
         self.player.update()
+
+        
 
         #一応これは残しておく.Bulletクラスのオブジェクトたちは生成されたときにbullets.appendでbulletsリストへ入っている。
         # このリストはBulletクラスのオブジェクトが入っているため、forでbulletという変数の箱にbulletsのリストを1つずつ入れ
@@ -214,9 +258,16 @@ class App:
         #上記のbulletクラスのupdate()の使い方を踏まえて,関数をつくる.
         #今後もBulletクラスのようなリストで管理しないといけないようなたくさんのオブジェクト生成が予想される場合,クラスごとに上記のfor文を
         #書かずに済むようにする.Enemyクラスなどを作るかもしれないしな
+        entities_is_crash(bullets, enemies)
         entities_update(bullets)
-
+        entities_update(enemies)
+        
+        
+        entities_cleanup(enemies)
         entities_cleanup(bullets)
+        
+
+        
         
         
 
@@ -253,9 +304,12 @@ class App:
         self.background.draw()
         self.player.draw()
 
+        
+
         entities_draw(bullets)
         # for bullet in bullets:
         #     bullet.draw()
+        entities_draw(enemies)
 
     def draw_game_over_scene(self):
         pyxel.text(screen_width // 7 * 3, screen_height // 2, f"Game Over", pyxel.COLOR_LIME)
